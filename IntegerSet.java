c6842094ca854cbbe8f0c989caaba5cbde82e533
Mori <kittiwat.po@ku.th>
2025-07-20 00:32:31 +0700

    stater code

---
+++ IntegerSet.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class IntegerSet {
+    ArrayList<Integer> number;
+
+    public IntegerSet(){
+        number = new ArrayList<>();
+
+
+    }
+
+    public void checkRep(){
+        if (number!=null) {
+            if (number.size()>1) {
+                
+            }
+        
+        }
+    }
+
+}
